335120e15738334c31bb719066ef8a0d0d1e30a1
Alejandro perea <Alejandro@Alejandros-MacBook-Pro.local>
2020-10-02 16:56:25 -0400

    articles in user profile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from articles.models import Post
+from django.contrib.auth.models import User
 
 @login_required()
 def userprofile(request):
-    context = {}
+    current_user = request.user
+    posts = Post.objects.all()
+    context ={
+        'my_posts' : current_user.post_set.all()
+    }
+
     return render(request, 'userprofile.html', context)
+
+
+
+
+
